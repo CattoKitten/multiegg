81cd75cf1bf65b266f799e406f5bff73e0a60087
CattoKitten <85854337+CattoKitten@users.noreply.github.com>
2021-08-28 14:54:57 +0700

    Adding files

---
+++ Elixir-Egg/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /home/container
+
+# Output elixir version
+elixir --version
+
+# For startup variables
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
