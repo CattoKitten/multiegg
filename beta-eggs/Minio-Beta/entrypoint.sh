c27ca43ff0077acdbf909045f9ebbeb61ffab240
CattoKitten <85854337+CattoKitten@users.noreply.github.com>
2021-08-30 17:21:25 +0700

    Adding minio beta image script

---
+++ beta-eggs/Minio-Beta/entrypoint.sh
@@ -0,0 +1,8 @@
+# Download minio binary (Wont download if exists)
+wget https://dl.min.io/server/minio/release/linux-amd64/minio
+
+# Change minio binary permission
+chmod +x minio
+
+# Run the server!
+MINIO_ROOT_USER={{MINIO_ROOT_USER}} MINIO_ROOT_PASSWORD={{MINIO_ROOT_PASSWORD}} ./minio server /home/container --console-address "{{MINIO_PORT}}"
