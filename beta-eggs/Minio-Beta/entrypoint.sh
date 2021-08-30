6658c81d8353e297333e2c57a5b49c9637674e84
CattoKitten <85854337+CattoKitten@users.noreply.github.com>
2021-08-30 17:42:20 +0700

    Adding some parameters to entrypoint.sh

--- beta-eggs/Minio-Beta/entrypoint.sh
+++ beta-eggs/Minio-Beta/entrypoint.sh
@@ -1,5 +1,5 @@
 # Download minio binary (Wont download if exists)
-wget https://dl.min.io/server/minio/release/linux-amd64/minio
+wget -N https://dl.min.io/server/minio/release/linux-amd64/minio
 
 # Change minio binary permission
 chmod +x minio
